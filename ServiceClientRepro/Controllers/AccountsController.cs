666b46ca455dd5fc9c0143532918258166988797
Robert Talos <robert.talos@hogia.se>
2021-10-12 10:33:23 +0200

    initial commit

---
+++ ServiceClientRepro/Controllers/AccountsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.PowerPlatform.Dataverse.Client;
+
+namespace ServiceClientRepro.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly ServiceClient _serviceClient;
+
+        public AccountsController(ServiceClient serviceClient)
+        {
+            _serviceClient = serviceClient;
+        }
+
+        [HttpGet("~/accounts")]
+        public IActionResult GetAccount()
+        {
+            using (var context = new CrmServiceContext(_serviceClient))
+            {
+                var account = new Account();
+                account.Name = "ABC";
+                account.Description = null;
+
+                context.AddObject(account);
+
+                context.SaveChanges();
+            }
+
+            return Ok();
+        }
+    }
+}
